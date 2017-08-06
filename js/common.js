6938c0ffb251c6f5606b749839cbf15fff021849
darkwoolf10 <woolftreuser@yandex.ru>
2017-08-06 23:44:54 +0300

    added header in work

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	$("#form").submit(function () {
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: $(this).serialize()
+		}).done(function() {
+			alert("Письмо отправлено!");
+		});
+		return false;
+	});
+});
